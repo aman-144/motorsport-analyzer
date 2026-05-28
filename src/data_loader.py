4839083cd126fea93d093eaa65b0b33ab8333e43
aman-144 <codingyakuza@gmail.com>
2026-05-28 07:56:42 +0530

    feat: FastF1 data loader - 2023 Bahrain GP loading successfully

---
+++ src/data_loader.py
@@ -0,0 +1,3 @@
+import fastf1
+session = fastf1.get_session(2023,'Bahrain', 'R') #ye srif address dega ki data yaha rkha hai
+session.load() #ye data load karega ya esa samjho ye data ko leke ayegga
