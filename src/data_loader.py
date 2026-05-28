63ff0d6e0c29f9a4ff7aa6dbade64ab020f6dc5c
aman-144 <codingyakuza@gmail.com>
2026-05-28 09:19:03 +0530

    feat: driver lookup by name - Hamilton returns 44/HAM

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,3 +1,8 @@
 import fastf1
 session = fastf1.get_session(2023,'Bahrain', 'R') #ye srif address dega ki data yaha rkha hai
 session.load() #ye data load karega ya esa samjho ye data ko leke ayegga
+for driver_number in session.drivers:
+    driver_info = session.get_driver(driver_number)
+    if driver_info['LastName']== 'Hamilton': #ye hamilton ke data ko filter karega
+        print(driver_info['DriverNumber']) #ye hamilton ke driver number ko print karega
+        print(driver_info['Abbreviation']) #ye hamilton ke abbreviation ko print karega
