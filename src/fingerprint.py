828b00efa0720d55dc9b6f6e88129246fed5c9af
aman-144 <codingyakuza@gmail.com>
2026-05-29 09:34:34 +0530

    feat: consistency score added - VER more consistent than HAM

--- src/fingerprint.py
+++ src/fingerprint.py
@@ -17,21 +17,22 @@ def get_average_time(data):
     data['Sector3Time'] = pd.to_timedelta(data['Sector3Time'])
 
     avg_Laptime = data['LapTime'].mean()
+    std_Laptime = data['LapTime'].std()
     avg_Sector1Time = data['Sector1Time'].mean()
     avg_Sector2Time = data['Sector2Time'].mean()
     avg_Sector3Time = data['Sector3Time'].mean()
-    return avg_Laptime, avg_Sector1Time, avg_Sector2Time, avg_Sector3Time
+    return avg_Laptime, std_Laptime, avg_Sector1Time, avg_Sector2Time, avg_Sector3Time
 
-ham_avg_Laptime, ham_avg_S1time, ham_avg_S2time, ham_avg_S3time = get_average_time(ham_data)
-ver_avg_Laptime, ver_avg_S1time, ver_avg_S2time, ver_avg_S3time = get_average_time(ver_data)
+ham_avg_Laptime, ham_std_Laptime, ham_avg_S1time, ham_avg_S2time, ham_avg_S3time = get_average_time(ham_data)
+ver_avg_Laptime, ver_std_Laptime, ver_avg_S1time, ver_avg_S2time, ver_avg_S3time = get_average_time(ver_data)
 
 
 print("=== FINGERPRINT COMPARISON ===")
 print(f"Avg Lap   — Hamilton: {ham_avg_Laptime} | Verstappen: {ver_avg_Laptime}")
+print(f"Std Lap   — Hamilton: {ham_std_Laptime} | Verstappen: {ver_std_Laptime}")
 print(f"Avg Sector 1  — Hamilton: {ham_avg_S1time} | Verstappen: {ver_avg_S1time}")
 print(f"Avg Sector 2  — Hamilton: {ham_avg_S2time} | Verstappen: {ver_avg_S2time}")
 print(f"Avg Sector 3  — Hamilton: {ham_avg_S3time} | Verstappen: {ver_avg_S3time}")
-
 
 
 
