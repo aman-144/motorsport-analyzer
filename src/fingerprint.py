8a4910b9336ef45ed4d2deab9add6547ae939ab7
aman-144 <codingyakuza@gmail.com>
2026-05-29 09:01:10 +0530

    feat: both drivers loaded - Hamilton and Verstappen data ready

--- src/fingerprint.py
+++ src/fingerprint.py
@@ -1,18 +1,20 @@
 import pandas as pd
 
-data = pd.read_csv('processed_data/hamilton_2023_bahrain.csv') 
-print(data)
+def load_driver_data(driver_last_name):
+    data = pd.read_csv(f'processed_data/{driver_last_name}_2023_bahrain.csv')
+    return data
 
-data['LapTime'] = pd.to_timedelta(data['LapTime'])
-avg_lap_time = data['LapTime'].mean() 
-print( "Average Lap Time:",avg_lap_time)
+ham_data = load_driver_data('hamilton')
+ver_data = load_driver_data('verstappen')
 
-data['Sector1Time']=pd.to_timedelta(data['Sector1Time'])
-data['Sector2Time']=pd.to_timedelta(data['Sector2Time'])
-data['Sector3Time']=pd.to_timedelta(data['Sector3Time'])
-avg_sector1_time = data['Sector1Time'].mean()
-avg_sector2_time = data['Sector2Time'].mean()
-avg_sector3_time = data['Sector3Time'].mean()
-print("Average Sector 1 Time:", avg_sector1_time)
-print("Average Sector 2 Time:", avg_sector2_time)
-print("Average Sector 3 Time:", avg_sector3_time)
+print (ham_data)
+print (ver_data)
+
+
+
+
+
+
+
+
+
